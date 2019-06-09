612f0b5d52122cfeec6c22d7322879898af459e8
Henrik Paul <henrik.paul@gmail.com>
2019-06-09 23:37:31 +0300

    Refactor api into more functions

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,6 +3,7 @@ import { HelloWorldMessage } from "@ts-react-express-starter/common/interfaces";
 
 // TODO: inject endpoints from config
 const API_BASE = isProd ? "/api" : "//localhost:8081";
+const get = (path: string) => window.fetch(API_BASE + path, { method: "GET" });
 
 export const getHelloWorld = (): Promise<HelloWorldMessage> =>
-  fetch(`${API_BASE}/`).then(response => response.json());
+  get("/").then(response => response.json());
