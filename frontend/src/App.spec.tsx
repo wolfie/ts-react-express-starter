4b143dcae439916aff20d52ed8b46b39bc9bcd4c
Henrik Paul <henrik.paul@gmail.com>
2019-11-13 19:48:24 +0200

    Upgrade TypeScript and Jest

--- frontend/src/App.spec.tsx
+++ frontend/src/App.spec.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import "jest-enzyme";
 
 describe("<App>", () => {
   beforeEach(() => {});
 
   it("should render a heading and a paragraph", () => {
     const app = shallow(<App />);
-    expect(app.find("h1").exists()).toBeTruthy();
-    expect(app.find("p").exists()).toBeTruthy();
+    const heading = app.find("h1");
+    const paragraph = app.find("p");
+    expect(heading).toExist();
+    expect(paragraph).toExist();
   });
 });
